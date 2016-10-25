19428f7798bb7614f9bff4f87da26277f9385537
Cyumus <carlosbg1995@gmail.com>
2016-10-25 14:08:52 +0300

    + Java API Gateway   + Commented all functions to make you able to understand them.

--- RaspberryPi-Code/ThWxERP/src/com/cyumus/thingworx/erp/RaspberryPiClient.java
+++ RaspberryPi-Code/ThWxERP/src/com/cyumus/thingworx/erp/RaspberryPiClient.java
@@ -1,10 +1,8 @@
 package com.cyumus.thingworx.erp;
-import java.util.Collection;
 
 import com.cyumus.thingworx.erp.things.SensorThing;
 import com.thingworx.communications.client.ClientConfigurator;
 import com.thingworx.communications.client.ConnectedThingClient;
-import com.thingworx.communications.client.things.VirtualThing;
 import com.thingworx.communications.common.SecurityClaims;
 
 public class RaspberryPiClient extends ConnectedThingClient {
@@ -12,39 +10,65 @@ public class RaspberryPiClient extends ConnectedThingClient {
 		super(config);
 	}
 	
-	public static void main(String[] args) throws Exception {	
+	public static void main(String[] args) throws Exception {
+		// We create the configuration stuff and blablabla~
 		ClientConfigurator config = new ClientConfigurator();
+		// Websocket~
 		config.setUri("ws://localhost:80/Thingworx/WS");
+		// Reconnect every 15 seconds...
 		config.setReconnectInterval(15);
 		
-		SecurityClaims claims = SecurityClaims.fromCredentials("user", "pass");
+		// Using of credentials...
+		SecurityClaims claims = SecurityClaims.fromCredentials("Arduino", "1234");
 		config.setSecurityClaims(claims);
 		
+		// The name of the Gateway
 		config.setName("RaspberryPiGateway");
+		// It's a SDK Type
 		config.setAsSDKType();
 		
+		// We ignore all these bothering SSL errors. 
 		config.ignoreSSLErrors(true);
-
-		int scanRate = 1000;
 		
+		// The delay of the Thing.
+		int delay = 1000;
+		
+		// We create  client that will use the Sensor.
+		// In this case, this is a virtual representation of the Raspberry Pi.
 		RaspberryPiClient client = new RaspberryPiClient(config);
 		
+		// We create a sensor, with its name, description, the id and the client that will use it.
 		SensorThing sensor = new SensorThing("Sensor", "A sensor", "SensorThing", client);
+		// We bind the client to the sensor.
+		// This is the same as if you give the sensor to someone in order to use it.
 		client.bindThing(sensor);
 		
 		try {
+			// We say to the client to start working.
 			client.start();
 		}
 		catch(Exception eStart) {
 			System.out.println("Initial Start Failed : " + eStart.getMessage());
 		}
 		
+		// While the client is working
 		while(!client.isShutdown()) {
+			// If the client is connected to Thingworx
 			if(client.isConnected()) {
-				sensor.setEmpty(Math.random()>=0.5);
+				// It makes the sensor to scan
+				client.scan(sensor);
+				// And it updates its values to Thingworx
 				sensor.processScanRequest();
 			}
-			Thread.sleep(scanRate);
+			Thread.sleep(delay);
 		}
+	}
+	/**
+	 * This function makes Raspberry Pi to communicate with Arduino board and obtain all
+	 * values of the sensor.
+	 * @param sensor The sensor
+	 */
+	private void scan(SensorThing sensor){
+		sensor.setEmpty(Math.random()>=0.5);
 	}
 }
--- RaspberryPi-Code/ThWxERP/src/com/cyumus/thingworx/erp/things/SensorThing.java
+++ RaspberryPi-Code/ThWxERP/src/com/cyumus/thingworx/erp/things/SensorThing.java
@@ -16,21 +16,38 @@ import com.thingworx.types.collections.ValueCollection;
 import com.thingworx.types.constants.CommonPropertyNames;
 
 @SuppressWarnings("serial")
+
+/**
+ * These annotations defines all the Thing virtual representation properties.
+ */
 @ThingworxPropertyDefinitions(properties = {	
 		@ThingworxPropertyDefinition(name="isEmpty", description="Is Empty?", baseType="BOOLEAN", category="Status", aspects={"isReadOnly:false"}),
 	})
 
-// Event Definitions
+/**
+ * These annotations defines all the Thing virtual representation events.
+ */
 @ThingworxEventDefinitions(events = {
 	@ThingworxEventDefinition(name="SensorFault", description="Sensor fault", dataShape="Sensor.Fault", category="Faults", isInvocable=true, isPropertyEvent=false)
 })
 
-// Steam Thing virtual thing class that simulates a Steam Sensor
+/**
+ * This class simulates a Sensor. It's, in fact, a virtual representation of a Sensor.
+ */
 public class SensorThing extends VirtualThing implements Runnable {
 	private boolean isEmpty;
 	private Thread _shutdownThread = null;
 	
-	
+	/**
+	 * The constructor of the Sensor. It creates the sensor with the name, description, id, and the client that uses it.
+	 * Then, it creates all the fault events that can occur in the future, if it fails.
+	 * It initialize the annotations and creates the default properties, events and services.
+	 * At the end, it initialize itself. 
+	 * @param name The name of the sensor.
+	 * @param description A simple description.
+	 * @param identifier A string of characters to identify it on Thingworx
+	 * @param client Who uses this Sensor.
+	 */
 	public SensorThing(String name, String description, String identifier, ConnectedThingClient client) {
 		super(name,description,identifier,client);
 		
@@ -42,12 +59,19 @@ public class SensorThing extends VirtualThing implements Runnable {
 		this.init();
 	}
 
-
+	/**
+	 * This function synchronize everything with Thingworx server.
+	 */
 	public void synchronizeState() {
 		super.synchronizeState();
 		super.syncProperties();
 	}
 	
+	/**
+	 * This function initializes the custom properties, events and services.
+	 * Then, it defines a new Data Shape, called SensorReadings, that can be returned to
+	 * Thingworx if called from there.
+	 */
 	private void init() 
 	{
 		initializeFromAnnotations();
@@ -57,46 +81,84 @@ public class SensorThing extends VirtualThing implements Runnable {
         defineDataShapeDefinition("SensorReadings", fields);
 	}
 	
+	/**
+	 * This kind of annotation defines the services. In this case, we are defining the GetSensorReadings
+	 * service, and we link it to the function GetSensorReadings().
+	 * Then, you will be able to call this Remote Service from Thingworx.
+	 * When you call this Remote Service, Thingworx will send an order to this Thread to run the
+	 * function linked to the Remote Service that it called, in this case, the Thread will run this function.
+	 * @return
+	 */
 	@ThingworxServiceDefinition( name="GetSensorReadings", description="Get Sensor Readings")
+	
+	/**
+	 * We define the return format. As we said before, we defined a DataShape called 'SensorReadings'.
+	 * Then, we use it as the return dataShape type
+	 */
 	@ThingworxServiceResult( name=CommonPropertyNames.PROP_RESULT, description="Result", baseType="INFOTABLE", aspects={"dataShape:SensorReadings"} )
-	public InfoTable GetSensorReadings() 
-	{		
+	
+	/**
+	 * This function is linked to the GetSensorReadings Remote Service.
+	 * It creates a SensorReadings InfoTable to send back to Thingworx.
+	 * It fills the InfoTable with the name of the Sensor and the value that
+	 * it scanned at that time.
+	 * @return The SensorReadings InfoTable filled with the name of the Sensor and its readings.
+	 */
+	public InfoTable GetSensorReadings(){		
 		InfoTable table = new InfoTable(getDataShapeDefinition("SensorReadings"));
-
 		ValueCollection entry = new ValueCollection();
-		
-		try 
-		{			
+		try{			
 			entry.clear();
-			entry.SetBooleanValue("isEmpty", false);
+			entry.SetStringValue("Name", this.getName());
+			entry.SetBooleanValue("isEmpty", this.isEmpty);
 			table.addRow(entry.clone());
 		} 
-		catch (Exception e) 
-		{
+		catch (Exception e){
 			e.printStackTrace();
 		}
-
 		return table;
 	}
 	
+	/**
+	 * This function can be used by the client to set the values that it scans.
+	 * @param iE
+	 */
 	public void setEmpty(boolean iE){
 		this.isEmpty = iE;
 	}
 	
+	/**
+	 * This function is used in a loop with a delay.
+	 * It updates all the values in the Thingworx server.
+	 */
 	@Override
 	public void processScanRequest() throws Exception {
 		super.processScanRequest();
-		this.scanDevice();
+		this.update();
 	}
 	
-	public void scanDevice() throws Exception {
+	/**
+	 * This function updates all the values in Thigworx.
+	 * @throws Exception
+	 */
+	public void update() throws Exception {
 		super.setProperty("isEmpty", this.isEmpty);
 		super.updateSubscribedProperties(15000);
 		super.updateSubscribedEvents(60000);
 	}
 
+	
+	/**
+	 * You can use this service to remotely shutdown the device.
+	 * @throws Exception
+	 */
 	@ThingworxServiceDefinition( name="Shutdown", description="Shutdown the client")
 	@ThingworxServiceResult( name=CommonPropertyNames.PROP_RESULT, description="", baseType="NOTHING")
+	/**
+	 * This function is linked with the Shutdown service.
+	 * It starts the shutdown Thread, and runs it.
+	 * @throws Exception
+	 */
 	public synchronized void Shutdown() throws Exception {
 		if(this._shutdownThread == null) {
 			this._shutdownThread = new Thread(this);
@@ -104,6 +166,11 @@ public class SensorThing extends VirtualThing implements Runnable {
 		}
 	}
 
+	/**
+	 * When you run the shutdown Thread,
+	 * it sleeps 1 second, it gets the client of itself
+	 * and shuts down it.
+	 */
 	@Override
 	public void run() {
 		try {
