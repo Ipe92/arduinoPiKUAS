5366b7f1ef00f537664f58f5b1b92843cb890ab0
Ipe92 <Ipe92@users.noreply.github.com>
2016-12-15 14:24:48 +0200

    Add files via upload
    NAV code

---
+++ NAV_gateway/NAV_gateway/MainProgram.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+//  http://ril-sql.rilling.local:7047/DynamicsNAV71/WS/Cronus%20Admin/Page/Job_Journal
+//  USE NAVUSER3
+//  The development challenge set up given in the beginning of the course was about managing the enterprise operations using
+//  the SBC's, wireless XBee technology and ethernet network to trigger a job (or order) to replenish the material bin in the
+//  warehouse whenever needed. The process ends up into the Dynamics NAV ERP. The Jobs part in Dynamics NAV is one possible
+//  functionality for us. Using jobs you can deliver a combination of goods, services and consultation for customer.
+//  Some further reading and material about NAV jobs:
+//  THIS CODE ONLY WORKS FROM THE COMPUTER WHERE DYNAMIC NAV IS INSTALLED!
+//  otherwise it doesn't recognize the remote name or web references
+//  It uses 'ril-sql.rilling.local'
+namespace NAV_gateway
+{
+    // We use this webreference for purchase order
+    using Purchase;
+
+    // Try to just get thingworx information without ERP
+
+    class MainProgram
+
+    {
+
+        static void Main(string[] args)
+        {
+            // This connects to thingworx and GETs ItemAmount
+            // Then if ItemAmount is too low Paramfunc gets executed
+            // It creates purchase order to ERP
+            //PollerCode poller = new PollerCode("172.16.28.122", 80, "/Thingworx/Things/Item A/Properties/ItemAmount/GET");
+            PollerCode poller = new PollerCode("10.30.50.161", 80, "Thingworx/Things/Item A/Properties/ItemAmount/GET");
+            
+            //PollerCode poller = new PollerCode("127.0.0.1", 8080, "");
+            poller.start();
+
+            /*
+            // This code will make a new purchase order for more screws
+            // ERP callback
+            Order_screws_Service screwService = new Order_screws_Service();
+            Order_screws orderScrews = new Order_screws();
+            // Another object for creating Purchase order line
+            Purchase_Order_Line[] p = new Purchase_Order_Line[10];
+
+            //Simple authentication and new values for Order_screws
+            //screwService.UseDefaultCredentials = true;
+            orderScrews.Buy_from_Vendor_No = ("1020");
+            orderScrews.Buy_from_Vendor_Name = ("Cronus Cardoxy Sales");
+            orderScrews.Status = Status.Released;
+
+            // Purchase order line code
+            p[0] = new Purchase_Order_Line();
+            p[0].Type = Type.Item;
+            p[0].TypeSpecified = true;
+            p[0].No = "6666A";
+            p[0].Description = ("Ruuveja");
+            p[0].Quantity = (900);
+            p[0].Location_Code = "ASSEMBLY";
+            p[0].QuantitySpecified = true;
+            p[0].Finished = true;
+            // Here we assign our Order_screws to use purchase order line
+            orderScrews.PurchLines = p;
+
+            // Creates a single record. Record object is overwritten with the version that is created by the page.
+            // soap exception after ~10 seconds
+            //string purchase_no = "106373";
+            //screwService.Delete(orderScrews.No = "purchase_no");
+            //int intpurch_no = Int16.Parse(purchase_no);
+            //intpurch_no++;
+            //purchase_no = intpurch_no.ToString();
+
+            // Prints some info into console (These values get into dynamic NAV)
+            Console.WriteLine("New purchase order created!");
+            Console.WriteLine(DateTime.Now.ToString("h:mm:ss tt"));
+            Console.WriteLine("Printing purchase order line");
+            Console.WriteLine("\tType: {0}", p[0].Type);
+            Console.WriteLine("\tNo: {0}", p[0].No);
+            Console.WriteLine("\tDescription: {0}", p[0].Description);
+            Console.WriteLine("\tQuantity: {0}", p[0].Quantity);
+            Console.WriteLine("\tLocation code: {0}", p[0].Location_Code);
+            Console.WriteLine("\tFinished: {0}", p[0].Finished);
+            */
+        }
+
+        private static callBack PurchaseOrderErp() {
+
+            Console.WriteLine("Thingworx ItemAmount");
+            return PurchaseOrderErp();
+        }
+    }
+}
---
+++ NAV_gateway/NAV_gateway/PollerCode.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.IO;
+//using JSON.NET;
+
+namespace NAV_gateway
+{
+
+    //Define delegate type for callback function
+    public delegate void callBack();
+
+    class PollerCode
+    {
+
+        private Poller poller;
+        private Thread pollingThread;
+        private ThreadStart tThreadStart;
+
+        /**
+         * @String ipAddr = ThingWorx computer IP (v4), 
+         * @int pPort = Thingworx Rest Api port,
+         * @String pRestRequest = URL for rest request, example: resource/key/value ... etc
+         * @callBack ErpCallBack = callback function, which sends a job request to the ERP
+         * 
+         * TODO make it poll thingworx once every 1-5 minute
+         */
+        public PollerCode(String ipAddr, int pPort, String pRestRequest)
+        {
+            poller = new Poller(ipAddr, pPort, pRestRequest);
+            tThreadStart = new ThreadStart(poller.Caller);
+            pollingThread = new Thread(tThreadStart);
+            pollingThread.Start();
+            //pollingThread.IsBackground = true;
+        }
+
+        /**
+         * Remember to start a thread! :P
+         */
+        public void start()
+        {
+            //tThreadStart.Start();
+        }
+
+        public class Poller
+        {
+            private string ip;
+            private int port;
+            private Uri url;
+            private String restRequest;
+            private Boolean okLevel;
+            private Boolean erpCalled;
+            private callBack ErpCallBack;
+
+            //connect to a server
+            public Poller(String ipAddr, int pPort, String pRestRequest)
+            {
+                ip = ipAddr;
+                port = pPort;
+                restRequest = pRestRequest;
+
+                url = new Uri("http://Arduino:1234@" + ip + ":" + port + "/" + restRequest);
+                okLevel = false;
+                erpCalled = false;
+            }
+
+            /**
+             * This checks from an item level server, if
+             * there is enough items. This calls the ERP
+             * to do a new job to fill the item bin.
+             * This doesn't do anything while the item level is too
+             * low after calling the ERP.
+             * */
+            public void Caller()
+            {
+                // test if Caller() method is used
+               while (true) 
+               // while (Thread.CurrentThread.IsAlive)
+                {
+                    // poll thingworx every 10 seconds
+                    // somehow doesn't work
+                    Thread.Sleep(TimeSpan.FromSeconds(10));
+
+                    String response;
+                    response = ReadRestResponse(url);
+                    okLevel = isLevelOk(response);
+
+                    //This calls ERP to open new job
+                    if (!erpCalled && !okLevel)
+                    {
+                        PurchaseOrderErp();
+                        erpCalled = true;
+                        // make thread sleep for 2 minutes after purchase order
+                        //Thread.Sleep(TimeSpan.FromMinutes(2));
+                        /*Console.WriteLine("Thread is sleeping for 2 minutes");
+                        Console.Write("Time when thread is active again:");
+                        DateTime sleepingTime = DateTime.Now.AddMinutes(2);
+                        Console.WriteLine(sleepingTime);*/
+                    }
+
+                    //This untriggers erpCalled
+                    //This is called after item level normalization
+                    else if (okLevel && erpCalled)
+                    {
+                        erpCalled = false;
+                    }
+
+
+
+
+                }
+            }
+
+            private String ReadRestResponse(Uri uri)
+            {
+                WebClient client = new WebClient();
+
+                //client.Credentials = new NetworkCredential("Arduino", 1234);
+
+                //So use THIS instead to send credentials RIGHT AWAY
+                string credentials = Convert.ToBase64String(
+                    Encoding.ASCII.GetBytes("Arduino" + ":" + "1234"));
+                client.Headers[HttpRequestHeader.Authorization] = string.Format(
+                    "Basic {0}", credentials);
+
+                client.Headers[HttpRequestHeader.Accept] = "Application/json";
+
+                Stream data = client.OpenRead(uri);
+               // client.Credentials();
+                StreamReader reader = new StreamReader(data);
+                string response = reader.ReadToEnd();
+                data.Close();
+                reader.Close();
+
+                return response;
+            }
+
+
+            /**
+             * This reads a response from ThingWorx
+             * ands decides, if there are too few of
+             * items
+             */
+            private Boolean isLevelOk(string response)
+            {
+                string[] list = response.Split(',');
+                string numberVar = "";
+        
+                foreach (string item in list){
+                    if (item.Contains("ItemAmount"))
+                    {
+                        numberVar = item.Split(':')[2];
+                        numberVar = numberVar.Replace("}", "");
+                        numberVar = numberVar.Replace(")", "");
+                        numberVar = numberVar.Replace("]", "");
+                    }            
+                    /*
+                    string[] datas = item.Split(':');
+
+                    //datas[0] is the name
+                    //datas[1] is the value
+                    Console.WriteLine(datas[0]);
+                    if (datas[0].Contains("ItemAmount"))
+                    {
+                        numberVar = datas[1].Replace(@"\", "");
+                        break;
+                    }*/
+                }
+                
+                
+                //dynamic data = JObject.Parse(response);
+                int screwLevel = Int16.Parse(numberVar);
+
+                if (screwLevel > 100)
+                {
+                    Console.WriteLine("Screw level is OK.");
+                    return true;
+
+                }
+                else
+                {
+                    Console.WriteLine("Screw level not OK.");
+                    return false;
+                }
+            }
+        }
+
+        private static void PurchaseOrderErp() {
+
+            Console.WriteLine("Thingworx ItemAmount");
+
+        }
+
+    }
+}
